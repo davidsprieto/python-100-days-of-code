851c0d5ee82f41ad7a838ff45c4eed4374649f66
David Prieto <david.s.prieto1@gmail.com>
2024-04-11 18:26:11 -0500

    completed turtle etch-a-sketch challenge

---
+++ days-12-to-23/day-19.py
@@ -0,0 +1,46 @@
+# Day 19/100 - Instances, State, and Higher Order Functions
+
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    turtle.forward(10)
+
+
+screen.onkey(key="w", fun=move_forwards)
+
+
+def move_backwards():
+    turtle.back(10)
+
+
+screen.onkey(key="s", fun=move_backwards)
+
+
+def move_counter_clockwise():
+    turtle.left(5)
+
+
+screen.onkey(key="a", fun=move_counter_clockwise)
+
+
+def move_clockwise():
+    turtle.right(5)
+
+
+screen.onkey(key="d", fun=move_clockwise)
+
+
+def clear_drawing():
+    turtle.home()
+    turtle.clear()
+
+
+screen.onkey(key="c", fun=clear_drawing)
+
+
+screen.listen()
+screen.exitonclick()
